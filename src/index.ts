0d1c448810021f22aefdc1e6ea982c37c11d4d32
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2026-01-31 00:27:17 +0530

    fileserver start with the express static middleware

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
+app.use(express.static("."));
+
 const startServer = () => {
     app.listen(PORT, () => {
-        console.log(`server is listening to the port ${PORT}`);
+        console.log(`server is running at http://localhost:${PORT}`);
     });
 }
 
