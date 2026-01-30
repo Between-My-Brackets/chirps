dc10d89cec884894bfbdc045b7d820f70476623b
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2026-01-31 01:02:29 +0530

    cleaner and more readable code no significant modification

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import express from "express";
-import { Request, Response } from "express";
+import {healthReadiness} from "./api/readiness.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use("/app", express.static("src/app"));
 
-app.get("/healthz", (req: Request, res: Response) => {
-    res.set("Content-Type", "text/plain; charset=utf-8");
-    res.send("OK");
-});
+app.get("/healthz", healthReadiness);
 
 const startServer = () => {
     app.listen(PORT, () => {
