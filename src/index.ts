2b8fdbcf90bcd7b41d52b514f5b7c9451cae1e4f
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2026-01-31 14:22:37 +0530

    added a loggin middleware that does not return the 2XX status code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import {healthReadiness} from "./api/readiness.js";
+import {middlewareLogResponses} from "./middlewares/logging.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use("/app", express.static("src/app"));
-
+app.use(middlewareLogResponses);
 app.get("/healthz", healthReadiness);
 
 const startServer = () => {
