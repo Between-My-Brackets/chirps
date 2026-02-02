8ca64a5609224fd72cb094fd1e65aba1c64c0277
Prajwal-Jagadeesh-Guliyavar <pjguliyavar10022004@gmail.com>
2026-02-02 09:03:15 +0530

    admin route for the extended endpoint

--- src/api/metrics.ts
+++ src/api/metrics.ts
@@ -10,3 +10,13 @@ export function resetMetrics(req: Request, res: Response){
     apiConfig.fileserversHits = 0;
     res.status(200).send();
 }
+
+export function adminGetMetrics(req: Request, res: Response){
+    res.set("Content-Type", "text/html; charset=utf-8");
+    res.send(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${apiConfig.fileserversHits} times!</p>
+  </body>
+</html>`);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import {healthReadiness} from "./api/readiness.js";
 import {middlewareLogResponses} from "./middlewares/logging.js";
-import {getMetrics, resetMetrics} from "./api/metrics.js";
+import {adminGetMetrics, getMetrics, resetMetrics} from "./api/metrics.js";
 import {middlewareMetricsInc} from "./middlewares/metrics.js";
 
 const app = express();
@@ -11,6 +11,8 @@ app.use("/app",middlewareMetricsInc, express.static("src/app"));
 app.use(middlewareLogResponses);
 
 app.get("/api/healthz", healthReadiness);
+app.get("/admin/metrics", adminGetMetrics);
+app.get("/admin/reset", resetMetrics);
 app.get("/api/metrics", getMetrics);
 app.get("/api/reset", resetMetrics);
 
